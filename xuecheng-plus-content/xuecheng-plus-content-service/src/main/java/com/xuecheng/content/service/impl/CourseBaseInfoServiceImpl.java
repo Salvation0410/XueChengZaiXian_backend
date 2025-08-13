9204faa9850688f78363e59c0d4749a95ee5712f
slavation <18879274824@163.com>
2025-08-13 15:46:19 +0800

    课程查询部分

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CourseBaseInfoServiceImpl.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CourseBaseInfoServiceImpl.java
@@ -3,6 +3,7 @@ package com.xuecheng.content.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 //import com.xuecheng.base.exception.XueChengPlusException;
+import com.sun.xml.internal.bind.v2.TODO;
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.mapper.CourseBaseMapper;
@@ -10,6 +11,8 @@ import com.xuecheng.content.mapper.CourseCategoryMapper;
 import com.xuecheng.content.mapper.CourseMarketMapper;
 //import com.xuecheng.content.model.dto.AddCourseDto;
 //import com.xuecheng.content.model.dto.CourseBaseInfoDto;
+import com.xuecheng.content.model.dto.AddCourseDto;
+import com.xuecheng.content.model.dto.CourseBaseInfoDto;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
 import com.xuecheng.content.model.po.CourseMarket;
@@ -20,18 +23,19 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.CollectionUtils;
 
-import javax.annotation.Resources;
 import java.time.LocalDateTime;
 import java.util.List;
 
-/**
+/*
+*
  * @author Mr.M
  * @version 1.0
  * @description TODO
  * @date 2023/2/12 10:16
- */
+
+*/
+
 @Slf4j
 @Service
 public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
@@ -69,14 +73,14 @@ public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
         return  courseBasePageResult;
     }
 
-  /*  @Transactional
+  @Transactional
     @Override
     public CourseBaseInfoDto createCourseBase(Long companyId, AddCourseDto dto){
 
         //参数的合法性校验
         if (StringUtils.isBlank(dto.getName())) {
 //            throw new RuntimeException("课程名称为空");
-            XueChengPlusException.cast("课程名称为空");
+            //XueChengPlusException.cast("课程名称为空");
         }
 
         if (StringUtils.isBlank(dto.getMt())) {
@@ -174,7 +178,7 @@ public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
         if(charge.equals("201001")){
            if(courseMarketNew.getPrice() ==null || courseMarketNew.getPrice().floatValue()<=0){
 //               throw new RuntimeException("课程的价格不能为空并且必须大于0");
-               XueChengPlusException.cast("课程的价格不能为空并且必须大于0");
+               //XueChengPlusException.cast("课程的价格不能为空并且必须大于0");
            }
         }
 
@@ -195,7 +199,8 @@ public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
         }
 
 
-    }*/
+    }
+
 
 
 
--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CourseCategoryServiceImpl.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CourseCategoryServiceImpl.java
@@ -1,6 +1,6 @@
-/*
 package com.xuecheng.content.service.impl;
 
+import com.sun.xml.internal.bind.v2.TODO;
 import com.xuecheng.content.mapper.CourseCategoryMapper;
 import com.xuecheng.content.model.dto.CourseCategoryTreeDto;
 import com.xuecheng.content.service.CourseCategoryService;
@@ -13,13 +13,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-*/
 /**
  * @author Mr.M
  * @version 1.0
- * @description TODO
+ * @description 课程分类相关服务层
  * @date 2023/2/12 14:49
- *//*
+ */
 
 @Slf4j
 @Service
@@ -32,33 +31,36 @@ public class CourseCategoryServiceImpl implements CourseCategoryService {
     public List<CourseCategoryTreeDto> queryTreeNodes(String id) {
         //调用mapper递归查询出分类信息
         List<CourseCategoryTreeDto> courseCategoryTreeDtos = courseCategoryMapper.selectTreeNodes(id);
+        //根据前端的接口数据 将courseCategoryTreeDtos封装成List<CourseCategoryTreeDto>类型 数据库和前端返回数据类型不一样
+        //先将List转成map key就是节点的id value就是CourseCategoryTreeDto对象 从而方便从map获取节点
 
-        //找到每个节点的子节点，最终封装成List<CourseCategoryTreeDto>
-        //先将list转成map，key就是结点的id，value就是CourseCategoryTreeDto对象，目的就是为了方便从map获取结点,filter(item->!id.equals(item.getId()))把根结点排除
-        Map<String, CourseCategoryTreeDto> mapTemp = courseCategoryTreeDtos.stream().filter(item -> !id.equals(item.getId())).collect(Collectors.toMap(key -> key.getId(), value -> value, (key1, key2) -> key2));
-        //定义一个list作为最终返回的list
+        Map<String,CourseCategoryTreeDto> mapTemp =courseCategoryTreeDtos.stream()
+                //排除根节点
+                .filter(item->!id.equals(item.getId()))
+                .collect(Collectors.toMap(key->key.getId(),value->value,(key1,key2)->key2));
+
+        //存放返回结果集
         List<CourseCategoryTreeDto> courseCategoryList = new ArrayList<>();
-        //从头遍历 List<CourseCategoryTreeDto> ，一边遍历一边找子节点放在父节点的childrenTreeNodes
-        courseCategoryTreeDtos.stream().filter(item -> !id.equals(item.getId())).forEach(item -> {
-            if (item.getParentid().equals(id)) {
-                courseCategoryList.add(item);
-            }
-            //找到节点的父节点
-            CourseCategoryTreeDto courseCategoryParent = mapTemp.get(item.getParentid());
-            if(courseCategoryParent!=null){
-                if(courseCategoryParent.getChildrenTreeNodes()==null){
-                    //如果该父节点的ChildrenTreeNodes属性为空要new一个集合，因为要向该集合中放它的子节点
-                    courseCategoryParent.setChildrenTreeNodes(new ArrayList<CourseCategoryTreeDto>());
+
+        //遍历List<CourseCategoryTreeDto> 一边遍历一边找子节点放在父节点的childrenTreeNodes
+        courseCategoryTreeDtos.stream()
+                .filter(item->!id.equals(item.getId()))
+                .forEach(item->{
+                    if(item.getParentid().equals(id)){
+                           courseCategoryList.add(item);
+                    }
+                //找到当前节点的父节点
+                CourseCategoryTreeDto courseCategoryParent = mapTemp.get(item.getParentid());
+                if(courseCategoryParent!=null){
+                    if(courseCategoryParent.getChildrenTreeNodes() == null){
+                        //该父节点的ChildrenTreeNodes属性空时new一个新的集合 放该节点的子节点
+                        courseCategoryParent.setChildrenTreeNodes(new ArrayList<CourseCategoryTreeDto>());
+                    }
+                    courseCategoryParent.getChildrenTreeNodes().add(item);
                 }
-                //到每个节点的子节点放在父节点的childrenTreeNodes属性中
-                courseCategoryParent.getChildrenTreeNodes().add(item);
-            }
 
-
-
-        });
+                });
 
         return courseCategoryList;
     }
 }
-*/
