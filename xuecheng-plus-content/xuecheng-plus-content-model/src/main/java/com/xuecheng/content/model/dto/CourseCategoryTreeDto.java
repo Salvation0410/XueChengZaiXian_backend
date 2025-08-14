fe62849114c588312e26e811854c73eb8bd184ed
slavation <18879274824@163.com>
2025-08-14 09:25:09 +0800

    新增课程部分 全局异常处理 JSR303校验

--- xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/content/model/dto/CourseCategoryTreeDto.java
+++ xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/content/model/dto/CourseCategoryTreeDto.java
@@ -1,20 +1,20 @@
 package com.xuecheng.content.model.dto;
 
-
 import com.xuecheng.content.model.po.CourseCategory;
 import lombok.Data;
 
 import java.util.List;
 
 /**
- * @ClassName CourseCategoryTreeDto
- * @Description
- * @Author
- * @Date 2025/8/13 9:37
+ * @author Mr.M
+ * @version 1.0
+ * @description TODO
+ * @date 2023/2/12 11:51
  */
-
 @Data
-public class CourseCategoryTreeDto extends CourseCategory {
-    //子节点
-    List<CourseCategoryTreeDto> childrenTreeNodes;
+public class CourseCategoryTreeDto extends CourseCategory implements java.io.Serializable {
+
+   //子节点
+   List<CourseCategoryTreeDto> childrenTreeNodes;
+
 }
