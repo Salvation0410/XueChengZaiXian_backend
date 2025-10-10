d542773afbf277f3199dcbb122d7cfe33cc4271e
slavation <18879274824@163.com>
2025-10-10 15:46:42 +0800

    springSecurity相关配置 认证相关配置

--- xuecheng-plus-media/xuecheng-plus-media-api/src/main/java/com/xuecheng/media/config/ResouceServerConfig.java
+++ xuecheng-plus-media/xuecheng-plus-media-api/src/main/java/com/xuecheng/media/config/ResouceServerConfig.java
@@ -10,40 +10,36 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 import org.springframework.security.oauth2.provider.token.TokenStore;
 
 /**
- * @description 资源服务配置
- * @author Mr.M
- * @date 2022/10/18 16:33
+ * @author Administrator
  * @version 1.0
- */
- @Configuration
- //开启资源服务（结合Oauth2）让本服务具备接收并校验access_token的能力
- @EnableResourceServer
- //启用方法级别的校验功能
- @EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true)
- public class ResouceServerConfig extends ResourceServerConfigurerAdapter {
+ **/
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true)
+public class ResouceServerConfig extends ResourceServerConfigurerAdapter {
 
 
-  //资源服务标识 由Auth服务的AuthorizationServer识别并标识
-  public static final String RESOURCE_ID = "xuecheng-plus";
+    //资源服务标识
+    public static final String RESOURCE_ID = "xuecheng-plus";
 
-  @Autowired
-  TokenStore tokenStore;
+    @Autowired
+    TokenStore tokenStore;
 
-  @Override
-  public void configure(ResourceServerSecurityConfigurer resources) {
-   resources.resourceId(RESOURCE_ID)//资源 id
-           .tokenStore(tokenStore)
-           .stateless(true);
-  }
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.resourceId(RESOURCE_ID)//资源 id
+                .tokenStore(tokenStore)
+                .stateless(true);
+    }
 
- @Override
- public void configure(HttpSecurity http) throws Exception {
-  http.csrf().disable()
-          .authorizeRequests()
-          //已经在gateway网关进行认证了
- //         .antMatchers("/r/**","/course/**").authenticated()//所有/r/**的请求必须认证通过 管控的资源路径
-          .anyRequest().permitAll()
-  ;
- }
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+//                .antMatchers("/r/**","/course/**").authenticated()//所有/r/**的请求必须认证通过
+                .anyRequest().permitAll()
+        ;
+    }
 
- }
+
+}
--- xuecheng-plus-media/xuecheng-plus-media-api/src/main/java/com/xuecheng/media/config/TokenConfig.java
+++ xuecheng-plus-media/xuecheng-plus-media-api/src/main/java/com/xuecheng/media/config/TokenConfig.java
@@ -1,6 +1,5 @@
 package com.xuecheng.media.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.token.TokenStore;
@@ -14,21 +13,12 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 public class TokenConfig {
 
-    //密钥
-    String SIGNING_KEY = "mq123";
-
-
-//    @Bean
-//    public TokenStore tokenStore() {
-//        //使用内存存储令牌（普通令牌）
-//        return new InMemoryTokenStore();
-//    }
-
-    @Autowired
-    private JwtAccessTokenConverter accessTokenConverter;
+    //jwt签名密钥,与认证服务保持一致
+    private String SIGNING_KEY = "mq123";
 
     @Bean
     public TokenStore tokenStore() {
+        //JWT令牌存储方案
         return new JwtTokenStore(accessTokenConverter());
     }
 
@@ -39,5 +29,9 @@ public class TokenConfig {
         return converter;
     }
 
-
+   /* @Bean
+    public TokenStore tokenStore() {
+        //使用内存存储令牌（普通令牌）
+        return new InMemoryTokenStore();
+    }*/
 }
